9e81fe2986ba3271dee6b400b28dc56e0a3ef628
PedroHBarauna <barauna38@gmail.com>
2023-03-13 15:00:03 -0300

    ajustando arquitetura

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule } from '@nestjs/config';
-import { UsuarioModule } from './modules/usuarios.modules';
+import { UsuarioModule } from './usuario/usuarios.modules';
 
 @Module({
   imports: [
